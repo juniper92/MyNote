c7072b8d87f548a6de6c2414b19f1f857300cc7a
Juniper <heyjuniper92@gmail.com>
2022-02-16 18:28:06 +0900

    first commit

---
+++ FutureNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FutureNote
+//
+//  Created by Juniper on 2022/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FutureNote/FutureNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureNoteApp.swift
+//  FutureNote
+//
+//  Created by Juniper on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct FutureNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
